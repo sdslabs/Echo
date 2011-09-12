cbc98b22c6ee5e122412fc7443313534bae09b99
rivendell <nishac92@gmail.com>
2011-09-13 02:05:57 +0530

    * EchoExtraction.scala basically makes use of the google books api, queries it, to get the required information about a book, given its title. * It returns the info as Map[String,String].

--- Echo/src/utils/EchoExtraction.scala
+++ Echo/src/utils/EchoExtraction.scala
@@ -1,7 +1,5 @@
-package utils
+package sdslabs.practice
 
-
-import scala.collection.mutable._
 import com.mongodb._
 import java.io.File
 import java.io.FileInputStream
@@ -32,165 +30,89 @@ import org.json.simple.JSONArray
 import java.io.BufferedReader
 import java.io.InputStreamReader
 import java.lang.NullPointerException
-
+import scala.collection.JavaConversions._
 
 class EchoExtraction {
-
-
-  def getTitle( f : File ): String = {
-	
-		var title_field : String = DublinCore.TITLE
-		
+  
+  def getTitle( f : File ): String = {	
+		var title_field : String = DublinCore.TITLE		
 		var stream : InputStream = new FileInputStream(f)            // converting from file to InputStream
 		var parser : Parser = new AutoDetectParser()                // to auto detect parser
 		var handler : ContentHandler = new BodyContentHandler(-1)      // in argument, maximam limit has to be specified
-		var context : ParseContext = new ParseContext()               
-		
-		context.set(classOf[Parser], parser)
-		
-		var metadata : Metadata = new Metadata()
-     
+		var context : ParseContext = new ParseContext() 	
+		context.set(classOf[Parser], parser)		
+		var metadata : Metadata = new Metadata()     
 		try {
 			parser.parse(stream, handler, metadata, context)
 		} 
 		finally {
 			stream.close()
 		}
-
 		var title : String = new String("")
 		metadata.names foreach ( name => {
 			if(name.compareTo(title_field) == 0 )    {
 				title = metadata.get(name)
 			}  
-		})
-		
+		})		
 		return title.toLowerCase()
   }
   
-  //---------end of the function getTitle--------------------
-  def searchGoogle(str : String): Map[String,String] = {
-	
-	var query : String = str.replaceAll(" ", "+").replaceAll(".pdf", "")
-			
-		try {
-			var url : String = "https://www.googleapis.com/books/v1/volumes?q=" + query
-			var is : InputStream = new URL(url).openStream()
-			var rd : BufferedReader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")))
-			var sb : StringBuilder = new StringBuilder()
-			var cp : Int = 0
-			cp = rd.read()
-			while (cp != -1) {
-			      sb.append(cp.asInstanceOf[Char])
-			      cp = rd.read()
-			}
-			
-			var json : JSONObject = JSONValue.parse(sb.toString()).asInstanceOf[JSONObject]
-			var item : String = json.get("items").toString
-			println(item)
-			var json2 : JSONArray = JSONValue.parse(item).asInstanceOf[JSONArray]
-			var book = json2.get(0).toString
-			var json3 : JSONObject = JSONValue.parse(book).asInstanceOf[JSONObject]
-			var id: String = json3.get("id").toString
-			
-			var tempMap : Map[String, String] = new HashMap[String, String]()
-			var tempURL : String = "http://www.google.com/books/feeds/volumes/" +id
-			var newTempMap : Map[String, String] = extractMetadata(tempURL)
-			println(newTempMap.toString)
-			
-			return newTempMap
-		}catch {
-		  case e : NullPointerException => {
-		    return null
-		  }
-		}
-		
-	}
-	
- 
-  //ParserConfigurationException, IOException
-  def extractMetadata( str :  String ): Map[String,String] = {
-	 {
-
-		var list : Map[String,String] = null
-
-		try {
-			var docBuilderFactory : DocumentBuilderFactory = DocumentBuilderFactory.newInstance()
-			var docBuilder: DocumentBuilder = docBuilderFactory.newDocumentBuilder()
-			var url : URL = new URL(str)
-			var urlConn : URLConnection = url.openConnection()
-			urlConn.setDoInput(true)
-			urlConn.setUseCaches(true)
-
-			var dis : DataInputStream = new DataInputStream(urlConn.getInputStream())
-			var doc : Document = docBuilder.parse (dis)
-			var fields_arr : Array[String] = Array("dc:title","dc:creator","dc:date","dc:description","dc:format","dc:identifier","dc:language","dc:publisher","dc:subject")
-			list = new HashMap[String,String]()
-
-			var i : Int = 0
-			while( i < 9 ) {
-				var ls : String = new String("")
-				var listOfNodes : NodeList = doc.getElementsByTagName(fields_arr(i))
-				var totalNodes : Int = listOfNodes.getLength()
-				var firstNodeElement : Element = null
-				var j : Int = 0
-				while(j < totalNodes ) {
-					firstNodeElement = listOfNodes.item(j).asInstanceOf[Element]
-					ls = ls + "  " + firstNodeElement.getTextContent()
-					j = j + 1
-				}
-				list.put(fields_arr(i).substring(3), ls.trim())
-				i = i + 1
-			}
-		}catch  {
-		  case e: SAXException => {  
-			  var x : Exception = e.getException ()
-			
-		  }
-		}
-
-		return list
-
-	}
-		
-}
-
-  def getInfo( name: String): Map[String, String] = {
+  def searchGoogle(str : String) : Map[String, String] = {
+    var query : String = str.replaceAll(" ", "+").replaceAll(".pdf", "")
+    var map : Map[String, String] = new HashMap[String, String]()
+    try {
+      var url : String = "https://www.googleapis.com/books/v1/volumes?q=" + query
+      var sb : StringBuilder = getJSONString(url)
+      var json : JSONObject = JSONValue.parse(sb.toString()).asInstanceOf[JSONObject]
+      var item : String = json.get("items").toString
+	  var json2 : JSONArray = JSONValue.parse(item).asInstanceOf[JSONArray]
+	  var book = json2.get(0).toString // taking only one book
+      var json3 : JSONObject = JSONValue.parse(book).asInstanceOf[JSONObject]     // all content of first book
+      var selflink : String = json3.get("selfLink").toString()
+      var sb1 : StringBuilder = getJSONString(selflink)
+      var jsonBook : JSONObject = JSONValue.parse(sb1.toString()).asInstanceOf[JSONObject]   // JSON file of the book
+      map = extractJSON(jsonBook)     
+    }    
+    return map
+  }
   
-    val gRes : Map[String,String] = searchGoogle(name)
-    if( gRes == null){
-      val ret = new HashMap[String,String]()
-      ret += ("title" -> name)
-      return ret
+  
+  def extractJSON(json : JSONObject) : Map[String, String] = {
+    var map : java.util.Map[String, String] = new java.util.HashMap[String, String]()
+    var str : String = json.get("volumeInfo").toString()
+    var json1 : JSONObject = JSONValue.parse(str).asInstanceOf[JSONObject]
+    var keyset : java.util.Set[String] = json1.keySet().asInstanceOf[java.util.Set[String]]
+    var iter : java.util.Iterator[String] = keyset.iterator()
+    while(iter.hasNext()) {
+      var tempKey : String = iter.next()
+      if (tempKey != "imageLinks") {
+        map.put(tempKey, json1.get(tempKey).toString())
+      } else {
+        var tempJsonString : String = json1.get(tempKey).toString()
+        var tempJson : JSONObject = JSONValue.parse(tempJsonString).asInstanceOf[JSONObject]
+        var tempKeySet : java.util.Set[String] = tempJson.keySet().asInstanceOf[java.util.Set[String]]
+        var tempIter : java.util.Iterator[String] = tempKeySet.iterator()
+        while(tempIter.hasNext()) {
+          var tempTempKey : String = tempIter.next()
+          map.put(tempTempKey, tempJson.get(tempTempKey).toString())
+        }
+      }
     }
-    
-    gRes += ("name" -> name)
-    return gRes
-  }
-  
-  def format( inp : DBObject): Map[String, String] = {
-    
-    val res : Map[String, String] = new HashMap[String, String]
-    
-    val set = inp.keySet.asInstanceOf[Set[String]]
-    set foreach ( e => {
-  			res.put(e, inp.get(e).asInstanceOf[String])
-  		}
-  	)
-    
-    if( inp.containsKey("cluster"))
-      res += ("cluster" -> inp.get("cluster").asInstanceOf[String])
-    return res
-  }
-  
-  def toDBObject( inp: Map[String,String]) : DBObject = {
-    
-    val obj : DBObject = new BasicDBObject()
-  	inp.keySet foreach ( e => {
-  			obj.put(e, inp.get(e).get)
-  		}
-  	)
-    return obj
+    var finalMap : Map[String, String] = map
+    return finalMap
   }
   
   
+  def getJSONString(url : String) : StringBuilder = {
+    var is : InputStream = new URL(url).openStream()
+	var rd : BufferedReader = new BufferedReader(new InputStreamReader(is, Charset.forName("UTF-8")))
+	var sb : StringBuilder = new StringBuilder()
+	var cp : Int = 0
+	cp = rd.read()
+	while (cp != -1) {
+	     sb.append(cp.asInstanceOf[Char])
+	     cp = rd.read()
+	}
+    return sb
+  }
 }
